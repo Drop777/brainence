70f75a2096b431ad8aeae88eaf22aca7899bb283
Pukhnichenko <victorpuhnichenko@gmail.com>
2019-10-31 01:16:22 +0200

    Updates

---
+++ precache-manifest.a35fa3d7a6afb787d0433a074a60909a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de71a8558b9d1ce16d032c5d28a72042",
+    "url": "/brainence/index.html"
+  },
+  {
+    "revision": "6b90fcee3fb3e7557b72",
+    "url": "/brainence/static/css/main.96a2a0f8.chunk.css"
+  },
+  {
+    "revision": "47c6bf77f12db6fa393f",
+    "url": "/brainence/static/js/2.4b91a361.chunk.js"
+  },
+  {
+    "revision": "6b90fcee3fb3e7557b72",
+    "url": "/brainence/static/js/main.68f0e54c.chunk.js"
+  },
+  {
+    "revision": "21b7467c373a660dada3",
+    "url": "/brainence/static/js/runtime-main.9a9c492e.js"
+  }
+]);
